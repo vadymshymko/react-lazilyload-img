8ce56c1966cb9e401e0a85e25f11751d7d7101bc
Vadym Shymko <vadymshymko@gmail.com>
2019-11-21 16:16:43 +0200

    Update packages and fix globalObject

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     library: 'ReactLazilyLoadImg',
     libraryTarget: 'umd',
+    globalObject: 'this',
   },
   externals: {
     react: {
